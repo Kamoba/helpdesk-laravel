e63a68691850c44d1a9277d495958b42b12cf9e0
Kamoba <35287463+Kamoba@users.noreply.github.com>
2024-09-16 20:30:50 -0600

    Update 2023_06_06_082020_create_tickets_table.php

--- database/migrations/2023_06_06_082020_create_tickets_table.php
+++ database/migrations/2023_06_06_082020_create_tickets_table.php
@@ -12,21 +12,24 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('tickets', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('priority_id')->index('priority_id');
-          //  $table->unsignedBigInteger('unit_id')->index('unit_id');  // cause "unit_id already exists" error for sqlite
-            $table->unsignedBigInteger('owner_id')->index('owner_id');
-            $table->unsignedBigInteger('problem_category_id')->index('problem_category_id');
-            $table->string('title');
-            $table->text('description');
-            $table->unsignedBigInteger('ticket_statuses_id')->index('ticket_statuses_id');
-            $table->unsignedBigInteger('responsible_id')->nullable()->index('responsible_id');
-            $table->timestamps();
-            $table->timestamp('approved_at')->nullable();
-            $table->timestamp('solved_at')->nullable();
-            $table->softDeletes();
-        });
+        
+        if (!Schema::hasTable('tickets')) {
+            Schema::create('tickets', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedBigInteger('priority_id')->index('priority_id');
+              //  $table->unsignedBigInteger('unit_id')->index('unit_id');  // cause "unit_id already exists" error for sqlite
+                $table->unsignedBigInteger('owner_id')->index('owner_id');
+                $table->unsignedBigInteger('problem_category_id')->index('problem_category_id');
+                $table->string('title');
+                $table->text('description');
+                $table->unsignedBigInteger('ticket_statuses_id')->index('ticket_statuses_id');
+                $table->unsignedBigInteger('responsible_id')->nullable()->index('responsible_id');
+                $table->timestamps();
+                $table->timestamp('approved_at')->nullable();
+                $table->timestamp('solved_at')->nullable();
+                $table->softDeletes();
+            });
+        }
     }
 
     /**
